544559dff8e67586dee0587c179b5776a134a653
Apogee <apogee@apogees-macbook-pro.local>
2017-08-14 12:16:17 +0530

    Initial Commit

---
+++ GRGeneraterAndReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GRGeneraterAndReader
+//
+//  Created by Apogee on 8/14/17.
+//  Copyright © 2017 Apogee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

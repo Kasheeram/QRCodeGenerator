4bda3ce3ca9f7d9d621e49801fb82e48c4fe6df5
kashee ram <kashee.nitd2016@gmail.com>
2017-08-17 00:56:37 +0530

    My First Commit

--- GRGeneraterAndReader/ViewController.swift
+++ GRGeneraterAndReader/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,AVCaptureMetadataOutputObjectsDelegate {
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    var video = AVCaptureVideoPreviewLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
+    
+    
 
+    @IBAction func button(_ sender: Any) {
+        if let myString = myTextField.text{
+            let data = myString.data(using: .ascii, allowLossyConversion: false)
+            let filter = CIFilter(name:"CIQRCodeGenerator")
+            filter?.setValue(data, forKey: "inputMessage")
+            let img = UIImage(ciImage:(filter?.outputImage)!)
+            myImageView.image = img
+        }
+    }
 
 }
 
